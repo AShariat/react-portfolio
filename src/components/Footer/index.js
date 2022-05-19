bbcc49d550473fc04e355f765dd5020e63cb709f
Amir Shariat <amirshariat1987@gmail.com>
2022-05-18 20:14:17 -0600

    footer done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer>
-      <h6>Footer</h6>
+      <a href="https://github.com/AShariat" target="_blank" rel="noreferrer">
+        <h6>
+          <FaGithub />
+        </h6>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/amirshariat"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h6>
+          <FaLinkedin />
+        </h6>
+      </a>
+      <a
+        href="https://www.facebook.com/amirshariat1987"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h6>
+          <FaFacebook />
+        </h6>
+      </a>
     </footer>
   );
 }
